50b17054a588df67890d5d94836376cd5ded3ab4
ＴｈｅＦｅｚｉａｋ 🚀💎 <162903090+thefeziak@users.noreply.github.com>
2025-02-09 09:56:25 +0100

    Update thefeziak.sh

--- thefeziak.sh
+++ thefeziak.sh
@@ -2,16 +2,9 @@
 
 ROOTFS_DIR=/home/container
 
-# Alpine settings
-ALPINE_VERSION="3.18"
-ALPINE_FULL_VERSION="3.18.3"
-APK_TOOLS_VERSION="2.14.0-r2"
+export PATH=$PATH:~/.local/usr/bin
+
 PROOT_VERSION="5.3.0"
-
-# Debian/Ubuntu settings
-DEBIAN_VERSION="bullseye"
-UBUNTU_VERSION="focal"
-
 ARCH=$(uname -m)
 
 if [ "$ARCH" = "x86_64" ]; then
@@ -19,72 +12,95 @@ if [ "$ARCH" = "x86_64" ]; then
 elif [ "$ARCH" = "aarch64" ]; then
   ARCH_ALT=arm64
 else
-  printf "Unsupported CPU architecture: ${ARCH}\n"
+  printf "Unsupported CPU architecture: ${ARCH}"
   exit 1
 fi
 
-if [ -e $ROOTFS_DIR/.installed ]; then
-  DISTRO_NAME=$(cat $ROOTFS_DIR/.installed)
-  printf "Using previously installed distribution: $DISTRO_NAME\n"
-else
-  printf "Choose a distribution:\n1. Alpine\n2. Debian\n3. Ubuntu\n"
-  read -r DISTRO
+if [ ! -e "$ROOTFS_DIR/.installed" ]; then
+echo "#######################################################################################"
+echo "#                                                                                     #"
+echo "#                                    > thefeziak <                                    #"
+echo "#                                                                                     #"
+echo "#                                      FREE VPS                                       #"
+echo "#                                                                                     #"
+echo "#                                                                                     #"
+echo "#######################################################################################"
 
-  case "$DISTRO" in
+  echo ""
+  echo "* [0] Debian"
+  echo "* [1] Ubuntu"
+  echo "* [2] Alpine"
+
+  read -p "Enter OS (0-2): " input
+
+  case $input in
+
+    0)
+      wget --no-hsts -O /tmp/rootfs.tar.xz \
+      "https://github.com/termux/proot-distro/releases/download/v3.10.0/debian-${ARCH}-pd-v3.10.0.tar.xz"
+      apt download xz-utils
+      deb_file=$(find "$ROOTFS_DIR" -name "*.deb" -type f)
+      dpkg -x "$deb_file" ~/.local/
+      rm "$deb_file"
+      tar -xJf /tmp/rootfs.tar.xz -C "$ROOTFS_DIR"
+      mkdir $ROOTFS_DIR/home/container/ -p
+
+      wget -O $ROOTFS_DIR/home/container/installer.sh \
+      "https://github.com/abdalla435/VPS-Pterodactyl-EGG/raw/main/private.sh"
+      wget -O $ROOTFS_DIR/home/container/.bashrc \
+      "https://github.com/abdalla435/VPS-Pterodactyl-EGG/raw/main/.bashrc"
+      wget -O $ROOTFS_DIR/home/container/style.sh \
+      "https://github.com/abdalla435/VPS-Pterodactyl-EGG/raw/main/style.sh"
+      ;;
+
     1)
-      printf "Selected: Alpine Linux\n"
-      curl -Lo /tmp/rootfs.tar.gz \
-      "https://dl-cdn.alpinelinux.org/alpine/v${ALPINE_VERSION}/releases/${ARCH}/alpine-minirootfs-${ALPINE_FULL_VERSION}-${ARCH}.tar.gz"
-      tar -xzf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
+      wget --no-hsts -O /tmp/rootfs.tar.gz \
+      "http://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.4-base-${ARCH_ALT}.tar.gz"
+      tar -xf /tmp/rootfs.tar.gz -C "$ROOTFS_DIR"
+      mkdir $ROOTFS_DIR/home/container/ -p
 
-      curl -Lo /tmp/apk-tools-static.apk "https://dl-cdn.alpinelinux.org/alpine/v${ALPINE_VERSION}/main/${ARCH}/apk-tools-static-${APK_TOOLS_VERSION}.apk"
-      curl -Lo $ROOTFS_DIR/usr/local/bin/proot "https://github.com/proot-me/proot/releases/download/v${PROOT_VERSION}/proot-v${PROOT_VERSION}-${ARCH}-static"
-      tar -xzf /tmp/apk-tools-static.apk -C /tmp/
-      /tmp/sbin/apk.static -X "https://dl-cdn.alpinelinux.org/alpine/v${ALPINE_VERSION}/main/" -U --allow-untrusted --root $ROOTFS_DIR add alpine-base apk-tools
-      chmod 755 $ROOTFS_DIR/usr/local/bin/proot
-      DISTRO_NAME="alpine"
+      wget -O $ROOTFS_DIR/home/container/installer.sh \
+      "https://github.com/abdalla435/VPS-Pterodactyl-EGG/raw/main/private.sh"
+      wget -O $ROOTFS_DIR/home/container/.bashrc \
+      "https://github.com/abdalla435/VPS-Pterodactyl-EGG/raw/main/.bashrc"
+      wget -O $ROOTFS_DIR/home/container/style.sh \
+      "https://github.com/abdalla435/VPS-Pterodactyl-EGG/raw/main/style.sh"
       ;;
+
     2)
-      printf "Selected: Debian\n"
-      curl -Lo /tmp/rootfs.tar.gz \
-      "https://deb.debian.org/debian/dists/${DEBIAN_VERSION}/main/installer-${ARCH_ALT}/current/images/netboot/netboot.tar.gz"
-      tar -xzf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
-
-      curl -Lo $ROOTFS_DIR/usr/local/bin/proot "https://github.com/proot-me/proot/releases/download/v${PROOT_VERSION}/proot-v${PROOT_VERSION}-${ARCH}-static"
-      chmod 755 $ROOTFS_DIR/usr/local/bin/proot
-      DISTRO_NAME="debian"
+      wget --no-hsts -O /tmp/rootfs.tar.gz \
+      "https://dl-cdn.alpinelinux.org/alpine/v3.18/releases/x86_64/alpine-minirootfs-3.18.3-${ARCH}.tar.gz"
+      tar -xf /tmp/rootfs.tar.gz -C "$ROOTFS_DIR"
+      mkdir $ROOTFS_DIR/etc/profile.d/ -p
+      
+      wget -O $ROOTFS_DIR/home/container/installer.sh \
+      "https://github.com/abdalla435/VPS-Pterodactyl-EGG/raw/main/private.sh"
+      wget -O $ROOTFS_DIR/home/container/.bashrc \
+      "https://github.com/abdalla435/VPS-Pterodactyl-EGG/raw/main/.bashrc"
+      wget -O $ROOTFS_DIR/home/container/style.sh \
+      "https://github.com/abdalla435/VPS-Pterodactyl-EGG/raw/main/style.sh"
       ;;
-    3)
-      printf "Selected: Ubuntu\n"
-      curl -Lo /tmp/rootfs.tar.gz \
-      "https://cloud-images.ubuntu.com/minimal/releases/${UBUNTU_VERSION}/release/ubuntu-minimal-cloudimg-${ARCH_ALT}-root.tar.xz"
-      tar -xf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
 
-      curl -Lo $ROOTFS_DIR/usr/local/bin/proot "https://github.com/proot-me/proot/releases/download/v${PROOT_VERSION}/proot-v${PROOT_VERSION}-${ARCH}-static"
-      chmod 755 $ROOTFS_DIR/usr/local/bin/proot
-      DISTRO_NAME="ubuntu"
-      ;;
     *)
-      printf "Invalid option. Exiting.\n"
+      echo "Invalid selection. Exiting."
       exit 1
       ;;
   esac
-
-  printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" > ${ROOTFS_DIR}/etc/resolv.conf
-  rm -rf /tmp/rootfs.tar.gz
-  echo "$DISTRO_NAME" > $ROOTFS_DIR/.installed
 fi
 
-clear
+if [ ! -e "$ROOTFS_DIR/.installed" ]; then
+    mkdir -p "$ROOTFS_DIR/usr/local/bin"
+    wget --no-hsts -O "$ROOTFS_DIR/usr/local/bin/proot" "https://github.com/proot-me/proot/releases/download/v${PROOT_VERSION}/proot-v${PROOT_VERSION}-${ARCH}-static"
+    chmod 755 "$ROOTFS_DIR/usr/local/bin/proot"
+fi
 
-$ROOTFS_DIR/usr/local/bin/proot \
+if [ ! -e "$ROOTFS_DIR/.installed" ]; then
+    printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" > "${ROOTFS_DIR}/etc/resolv.conf"
+    rm -rf /tmp/rootfs.tar.xz /tmp/sbin
+    touch "$ROOTFS_DIR/.installed"
+fi
+
+"$ROOTFS_DIR/usr/local/bin/proot" \
 --rootfs="${ROOTFS_DIR}" \
---link2symlink \
---kill-on-exit \
---root-id \
---cwd=/root \
---bind=/proc \
---bind=/dev \
---bind=/sys \
---bind=/tmp \
-/bin/sh
+-0 -w "/root" -b /dev -b /sys -b /proc -b /etc/resolv.conf --kill-on-exit \
+/bin/bash
